3347aecb152360f62c7a5e940703c977f1a1966a
yuqi <arjun.sardjoe.missier@live.nl>
2019-11-01 14:12:33 +0100

    test split; added cat 4

--- src/main.py
+++ src/main.py
@@ -1,18 +1,11 @@
-import numpy as np
 from time import time
 from sys import argv
+from sklearn.metrics import classification_report, accuracy_score
 from fd.dir import Dir
 from exceptions import NotAllExpectedExercisesPerformedError
 from configuration import Configuration as C
 from model.exercise import Exercise, Statistics
-from model.meta import Filter
 from ml.linear.logistic import LogisticRegression
-
-
-def filter_files(filter: Filter, files: [Exercise]):
-    for fil in filter.compiled:
-        files = [f for f in files if fil(f)]
-    return files
 
 
 if __name__ == '__main__':
@@ -26,14 +19,18 @@ if __name__ == '__main__':
 
     """Parse data"""
     _start = time()
-    dirs = [
-        Dir("data/Category_1"),
-        Dir("data/Category_2"),
-        Dir("data/Category_3")
-    ]
+    dirs = {
+        1: Dir("data/Category_1"),
+        2: Dir("data/Category_2"),
+        3: Dir("data/Category_3"),
+        4: Dir("data/Category_4")
+    }
+    dir_file_count = {1: None, 2: None, 3: None, 4: None}
     exercises = {}
-    for d in dirs:
-        for f in d.get_files_recursively([]):
+    for cat, d in dirs.items():
+        files = d.get_files_recursively([])
+        dir_file_count[cat] = len(files)
+        for f in files:
             key = "%s_%s" % (f.meta.cat, f.meta.pat)
             if exercises.get(key) is None:
                 exercises[key] = [f.exec(Exercise)]
@@ -41,9 +38,26 @@ if __name__ == '__main__':
                 exercises[key].append(f.exec(Exercise))
     print("Parsing data took %f seconds" % (time()-_start))
 
+    """Determine test patients"""
+    split = C.get('test_split')
+    test_patients = list()
+    test_patients_count = {1: 0, 2: 0, 3: 0, 4: 0}
+    for cat_pat, e in exercises.items():
+        _split = cat_pat.split('_', 1)
+        cat = int(_split[0])
+
+        needed = int(dir_file_count[cat] * (split/100))
+        if test_patients_count[cat] >= needed:
+            continue
+
+        test_patients.append(cat_pat)
+        test_patients_count[cat] += len(e)
+
     """Reduce data points"""
     data = list()
+    test_data = list()
     indicator = list()
+    test_indicator = list()
     for cat_pat, e in exercises.items():
         s = Statistics(e)
         try:
@@ -55,14 +69,21 @@ if __name__ == '__main__':
         for c in combinations:
             _tmp = []
             [_tmp.extend(ex.reduced) for ex in c]
-            data.append(_tmp)
-            indicator.append(c[0].meta.cat)
-    print(len(data))
-    print(len(indicator))
+
+            if cat_pat in test_patients:
+                test_data.append(_tmp)
+                test_indicator.append(c[0].meta.cat)
+            else:
+                data.append(_tmp)
+                indicator.append(c[0].meta.cat)
+    print(len(data), len(test_data))
+    print(len(indicator), len(test_indicator))
 
     """Train model"""
     model = LogisticRegression(solver="lbfgs", multi_class="auto", max_iter=2000, n_jobs=-1, verbose=1)
     model.train(data, indicator)
 
     """Assert functionality"""
-
+    test_pred = model.predict(test_data)
+    print(classification_report(test_indicator, test_pred, digits=3))
+    print(accuracy_score(test_indicator, test_pred, normalize=True, sample_weight=None))
